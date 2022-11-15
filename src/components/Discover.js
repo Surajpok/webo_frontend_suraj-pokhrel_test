651a7e1c36c4dc4c4f960ed0c49a666b3f505c6b
Suraj Pokhrel <Pokhrel004@gmail.com>
2022-11-15 20:24:05 +0545

    svg added on discover component

--- src/components/Discover.js
+++ src/components/Discover.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import "../assets/scss/Discover.scss"
+import Netsvg from "../assets/images/net.svg"
+import Conesvg from "../assets/images/cone.svg"
+import ConeOne from "../assets/images/cone1.svg"
+import Jigjag from "../assets/images/jigjag.png"
+import JigjagOne from "../assets/images/jigjag1.png"
+
+
+
 const Discover = () => {
     return (
         <div className="discover_container">
             <div className="discover_wrapper">
+                <img className="net_svg" src={Netsvg} alt="" />
+                <img className="cone_svg" src={Conesvg} alt="" />
+                <img className="cone_one_svg" src={ConeOne} alt="" />
+                <img className="jigjag_svg" src={Jigjag} alt="" />
+                <img className="jigjag_one_svg" src={JigjagOne} alt="" />
                 <div className="discover_title">
                     <h2>Discover a better way to manage spendings</h2>
                 </div>
